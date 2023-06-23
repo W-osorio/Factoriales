df3c29fa839f734a68eecd50a816e52cd1596908
W-osorio <111244407+W-osorio@users.noreply.github.com>
2023-06-22 21:37:36 -0400

    Add files via upload

---
+++ Factorial/Untitled-1.js
@@ -0,0 +1,8 @@
+//factorial-for.js
+const factorial = 10
+let num = 1;
+for (let i = 0; i < factorial ; i++){
+    num = num * (factorial - i) ;
+}
+console.log(num)
+
---
+++ Factorial/Untitled-2.js
@@ -0,0 +1,7 @@
+//factorial-while.js
+let = n=10, i=1; fact=1
+while(i<=n){
+    fact = fact*i
+    i++
+}
+console.log(fact)
